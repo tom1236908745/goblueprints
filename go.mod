d81fc913aefbc7c01794d9b52fba0d5bd7d3b936
tom1236908745 <tom1233908745@gmail.com>
2021-09-13 18:03:32 +0900

    add mongodbとTwitterAPIの接続(５章完)

--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.16
 
 require (
 	github.com/clbanning/x2j v0.0.0-20191024224557-825249438eec // indirect
+	github.com/garyburd/go-oauth v0.0.0-20180319155456-bca2e7f09a17 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/joeshaw/envdecode v0.0.0-20200121155833-099f1fc765bd // indirect
 	github.com/nsqio/go-nsq v1.0.8 // indirect
 	github.com/stretchr/codecs v0.0.0-20170403063245-04a5b1e1910d // indirect
 	github.com/stretchr/gomniauth v0.0.0-20170717123514-4b6c822be2eb // indirect
@@ -13,5 +15,6 @@ require (
 	github.com/stretchr/stew v0.0.0-20130812190256-80ef0842b48b // indirect
 	github.com/stretchr/tracer v0.0.0-20140124184152-66d3696bba97 // indirect
 	github.com/ugorji/go v1.2.6 // indirect
+	golang.org/x/net v0.0.0-20210908191846-a5e095526f91 // indirect
 	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
 )
--- twittervotes/main.go
+++ twittervotes/main.go
@@ -1,2 +1,96 @@
 package main
-func main(){}
+
+import (
+	"github.com/nsqio/go-nsq"
+	"gopkg.in/mgo.v2"
+	"log"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+)
+
+func main() {
+	var stoplock sync.Mutex
+	stop := false
+	stopChan := make(chan struct{}, 1)
+	signalChan := make(chan os.Signal, 1)
+	go func() {
+		<-signalChan
+		stoplock.Lock()
+		stop = true
+		stoplock.Unlock()
+		log.Println("停止します...")
+		stopChan <- struct{}{}
+		closeConn()
+	}()
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+
+	if err := dialdb(); err != nil {
+		log.Fatalln("MongoDBへのダイヤルに失敗しました:", err)
+	}
+	defer closedb()
+	// 処理を開始します
+	votes := make(chan string) // 投票結果のためのチャンネル
+	publisherStoppedChan := publishVotes(votes)
+	twitterStoppedChan := startTwitterStream(stopChan, votes)
+	go func() {
+		for {
+			time.Sleep(1 * time.Minute)
+			closeConn()
+			stoplock.Lock()
+			if stop {
+				stoplock.Unlock()
+				break
+			}
+			stoplock.Unlock()
+		}
+	}()
+	<-twitterStoppedChan
+	close(votes)
+	<-publisherStoppedChan
+}
+
+var db *mgo.Session
+
+func dialdb() error {
+	var err error
+	log.Println("MongoDBにダイヤル中: localhost")
+	db, err = mgo.Dial("localhost")
+	return err
+}
+func closedb() {
+	db.Close()
+	log.Println("データベース接続が閉じられました")
+}
+
+type poll struct {
+	Options []string
+}
+
+func loadOptions() ([]string, error) {
+	var options []string
+	iter := db.DB("ballots").C("polls").Find(nil).Iter()
+	var p poll
+	for iter.Next(&p) {
+		options = append(options, p.Options...)
+	}
+	iter.Close()
+	return options, iter.Err()
+}
+func publishVotes(votes <- chan string) <- chan struct{} {
+	stopchan := make(chan struct{}, 1)
+	pub, _ := nsq.NewProducer("localhost:4150", nsq.NewConfig())
+	go func() {
+		for vote := range votes {
+			pub.Publish("votes", []byte(vote)) //投票内容をパブリッシュ
+		}
+		log.Println("Publisher: 停止中です")
+		pub.Stop()
+		log.Println("publisher: 停止しました")
+		stopchan <- struct{}{}
+	}()
+	return stopchan
+}
+
